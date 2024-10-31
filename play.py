db834cea5728bda8ef6f9c30800546da6a2eba83
Amirreza Kamali <amirrezakamali5382@gmail.com>
2024-10-31 19:18:41 +0330

    first commit

---
+++ play.py
@@ -0,0 +1,33 @@
+import graphics.engine
+import rubics
+
+distance = 20
+scale = 60
+
+cube = rubics.rubicsCube()
+points, triangles = cube.readPoints, cube.readTriangles
+
+world = graphics.engine.Engine3D(points, triangles, scale=scale, distance=distance, width=400, height=600, title="Rubik's cube")
+world.rotate('y', -20)
+world.rotate('x', -20)
+world.render()
+
+def interact(event):
+    if event.keysym in ['r', 'l', 'u', 'd', 'f', 'b', 'R', 'L', 'U', 'D', 'F', 'B']:
+        k = event.keysym
+        exec('cube.' + k)
+        triangles = cube.readTriangles
+
+        world.clear()
+        world.writeTriangles(triangles)
+        world.render()
+    elif event.keysym == 'n':
+        cube.randomize(20)
+        triangles = cube.readTriangles
+
+        world.clear()
+        world.writeTriangles(triangles)
+        world.render()
+        
+world.screen.window.bind('<Key>', interact)
+world.screen.window.mainloop()
